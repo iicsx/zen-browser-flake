f28cce3b59e60cc73190f87a647c3e8d652d63ac
nex <maxinies00@gmail.com>
2025-03-24 13:09:19 +0100

    update(flake): run flake update, bump browser version

--- flake.nix
+++ flake.nix
@@ -9,15 +9,15 @@
     let
       system = "x86_64-linux";
       base = "linux-x86_64";
-      version = "1.10b";
+      version = "1.10.1b";
       downloadUrl = {
         "specific" = {
 	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.${base}.tar.xz";
-	  sha256 = "sha256:06v8caplc3qakqc9ifyfr0zmzpg83m86kc8yy8yaln77hxvw7lbz";
+	  sha256 = "sha256:0098qm8zvy1y0apb84f0nnik137jwiwfsjlgdypv95xz8pndcwfw";
 	};
 	"generic" = {
 	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.xz";
-	  sha256 = "sha256:06v8caplc3qakqc9ifyfr0zmzpg83m86kc8yy8yaln77hxvw7lbz";
+	  sha256 = "sha256:0098qm8zvy1y0apb84f0nnik137jwiwfsjlgdypv95xz8pndcwfw";
 	};
       };
 
